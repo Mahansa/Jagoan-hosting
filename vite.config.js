183f445ccc869a96abaa470abbd80ac72145a812
Mahansa <hansagamers11@gmail.com>
2025-08-19 15:39:49 +0700

    setup deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ganti "sekolah-web" dengan nama repo kamu
 export default defineConfig({
   plugins: [react()],
+  base: "/Jagoan-hosting/",
 })
